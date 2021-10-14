bb0f44a9f368536682488c3b1203d1cf7ddba67f
fabirino <fabalexandre8@gmail.com>
2021-10-14 11:37:44 +0100

    che che

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("che che che");
     }
 }
